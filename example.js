922c227338f3a5e846f435eb8a5d0f3979527a38
ych-wang <91431256+ych-wang@users.noreply.github.com>
2021-12-20 15:54:21 +0100

    Add files via upload

---
+++ example.js
@@ -0,0 +1,125 @@
+const demolist = [
+    {
+        "type": "Feature",
+        "properties": {
+          "person": "Anton S--- ",
+          "date": 'none',
+          "col_loc": "Barbados",
+          "origin": "Zurich",
+          "activites": ['military, trading'],
+          "full_entry": " After 1800, Anton Schulthess from a Zurich merchant and banking family served as captain in the British Royal-African-Corps in Barbados. In 1814, his elder brother, Colonel Paravicin Schulthess (1757-1843), applied to the British ambassador for a death certificate for his brother, who had apparently deceased some years earlier."
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+                -59.5458984375,
+                13.282718960896405
+            ],
+            [
+              8.536376953125,
+              47.3834738721015
+            ]
+          ]
+        }
+    },
+      {
+        "type": "Feature",
+        "properties": {
+          "person": "Anton Schulthess",
+          "date": 'none',
+          "col_loc": "Barbados",
+          "origin": "Zurich",
+          "activites": ['military'],
+          "full_entry": " After 1800, Anton Schulthess from a Zurich merchant and banking family served as captain in the British Royal-African-Corps in Barbados. In 1814, his elder brother, Colonel Paravicin Schulthess (1757-1843), applied to the British ambassador for a death certificate for his brother, who had apparently deceased some years earlier."
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+                -59.5458984375,
+                13.282718960896405
+            ],
+            [
+              8.536376953125,
+              47.3834738721015
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "person": "Hans Rudolf Zeller",
+          "date": "1639-1700",
+          "col_loc": "Jamaica",
+          "origin": "Zurich",
+          "activites": ['slave owner'],
+          "full_entry": "Hans Rudolf Zeller (1639-1700) and Hans Heinrich Hauser (1638-1683) from Zurich were Anglican clergymen in Jamaica. They probably owned slaves and Zeller complained about the Quakers› antislavery activities."
+        
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              -77.36572265625,
+              18.166730410221938
+            ],
+            [
+              8.54736328125,
+              47.3834738721015
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "person": "Hans from lausanne",
+          "date": "1639-",
+          "col_loc": "Jamaica",
+          "origin": "Lausanne",
+          "activites": ['slave owner'],
+          "full_entry": "some one from Lausanne"
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              -77.36572265625,
+              18.166730410221938
+            ],
+            [
+                6.6323,
+                46.5197
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "person": "hans from bern",
+          "date": "1639-",
+          "col_loc": "Jamaica",
+          "origin": "Bern",
+          "activites": ['slave owner'],
+          "full_entry": "some one from bern"
+        },
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              -77.36572265625,
+              18.166730410221938
+            ],
+            [
+              7.4474,
+              46.9480
+            ]
+          ]
+        }
+      }
+
+    ]
+  
